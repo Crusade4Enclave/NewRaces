94bc41ab9d5768cc9667ed332060cf5a9d1b12c0
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2022-06-02 21:01:18 -0500

    added flight check

--- src/engine/objects/PlayerCharacter.java
+++ src/engine/objects/PlayerCharacter.java
@@ -4781,8 +4781,11 @@ public void dismissNecroPets() {
 			}finally{
 				this.updateLock.writeLock().unlock();
 			}
+			if(AbstractCharacter.CanFly(this) == false && this.altitude > 0)
+			{
+				GroundPlayer(this);
+			}
 		}
-
 	}
 	@Override
 	public void updateFlight() {
