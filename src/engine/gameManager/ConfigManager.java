45ab56e98d65b819eab67220d6d8226356e143b9
MagicBot <MagicBot@magicbane.com>
2022-06-26 11:05:37 -0400

    More detail when missing a config entry.

--- src/engine/gameManager/ConfigManager.java
+++ src/engine/gameManager/ConfigManager.java
@@ -103,7 +103,7 @@ public enum ConfigManager {
                 Logger.info(configSetting.name() + ":" + configSetting.getValue());
             else {
                 Logger.error("Missing Config: " + configSetting.name());
-                Logger.error("This codebase expects >= MagicBox v1.3 " + configSetting.name());
+                Logger.error("This codebase requires >= MagicBox v1.3");
                 Logger.error("docker pull magicbane/magicbox:latest");
                 return false;
             }
